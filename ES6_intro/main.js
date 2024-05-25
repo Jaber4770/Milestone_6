f7d248b72f315104cfa9b30363e87aed9818bc55
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-05-25 12:59:11 +0300

    problem 1 done

---
+++ ES6_intro/main.js
@@ -0,0 +1,13 @@
+function multiply(pera1, pera2, pera3) {
+    return pera1 * pera2 * pera3;
+}
+
+console.log(multiply(1,2,3));
+
+let threeline = `I am a web developer.
+I love to code. 
+I love to eat biryani.`;
+console.log(threeline);
+
+let arrowFunction = (pera1, pera2 = 5) => pera1 + pera2;
+console.log(arrowFunction(5));
