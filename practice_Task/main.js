dddc035b50b292161c91fc21f6d1e56b5d7cbdb9
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-05-31 21:03:58 +0300

    joke data

--- practice_Task/main.js
+++ practice_Task/main.js
@@ -52,3 +52,40 @@ setTimeout(() => {
     clearInterval(jokeInterval);
 
 }, 10000);
+
+
+
+/* 
+
+Task - 3
+Write an async/await function that fetch data from an api and logs a message.
+
+Input:
+https://v2.jokeapi.dev/joke/Programming?type=single
+
+
+Note: you can use an api of your choice
+Output:
+Two SQL tables sit at the bar. A query approaches and asks "Can I join you?"
+
+
+Note: output may change according to the api you used
+
+*/
+
+const joke = async () => {
+    const res = await fetch('https://v2.jokeapi.dev/joke/Programming?type=single');
+    const data = await res.json();
+    const jokeData = data.joke;
+    console.log(data);
+    showJoke(jokeData);
+}
+function showJoke(jokeData){
+    const jokeContainer = document.getElementById('joke');
+    const paragraph = document.createElement('p');
+    paragraph.innerText = jokeData
+    jokeContainer.appendChild(paragraph);
+    
+}
+
+joke();
